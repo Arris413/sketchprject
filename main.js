b4dc960bb1cf2a7f670d7f7347f1d993cd7f8658
Arris413 <91161613+Arris413@users.noreply.github.com>
2023-04-27 17:44:10 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+
+timer_counter() 
+    {
+        variable:0;
+    }
+    timer_check()
+    {
+
+    }
+    drawn_sketch()
+    {
+        string
+    }
+    answer_holder()
+    {
+
+    }
+    score()
+    {
+        variable:0;
+    }
+
